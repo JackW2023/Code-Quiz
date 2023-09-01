103c5f6d29a3be15577e7b115dccf27bf6edf5ed
JW <daxon2012@gmail.com>
2023-09-01 12:37:22 -0700

    Added options for the user to choice their penelty

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -55,7 +55,6 @@ function generateQuiz() {
     }
     // checking to see if it worked
     console.log(index)
-
     // If the if statement doesn't get trigger it will run the following code
     // questionContainer.classList.remove("hide");
     questionContainer.classList.add("show");
@@ -70,6 +69,7 @@ function generateQuiz() {
     // because the question.js was ran before script js know the const questions exist
     // This line ultimately will run through all the question
     questionEl.textContent = questionsList[index].question;
+    
     // The choiceBtn was declare and I retrieve the choice buttons placeholder from the HTML
     // The .innerHTML property will set or get the HTML content within the element
     // When the .innerHTML is assign a string it will replace the existing HTML contents
@@ -156,6 +156,9 @@ function checkAnswers (userChoice) {
         // checking to see if it work
         console.log(score)
     } else {
+        // need to put the variable here because this is where the variable will be use cannot put it in global scope
+        // in if else statement it won't look for it if it doesn't exist
+        // declaring confirms, prompt, or alert also execute when you declare them
         const selection = confirm("Penalty! Do you want to deduct points? \n OK: 1 point will be deducted \n Cancel: 5s will be deducted")
         if (selection) {
             score--
